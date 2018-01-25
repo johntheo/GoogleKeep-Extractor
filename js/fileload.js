db93b51512bd3447a30db940c1a1289428da4423
johntheo <john.theo.souza@gmail.com>
2018-01-25 20:19:56 -0200

    File Reader done

--- js/fileload.js
+++ js/fileload.js
@@ -1,30 +1,57 @@
-var notes = []
+var notes = [];
+var output = [];
+
+var processedCount=0; // global variable
+var totalFiles = 0; // global variable
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
     var textType = /text.*/;
+    totalFiles = files.length;
 
     // files is a FileList of File objects. List some properties.
-    var output = [];
+    var result = "";
+    var note = new Object();
+
     for (var i = 0, f; f = files[i]; i++) {
-        var result = "";
+        note = new Object();
         if (f.type.match(textType)) {
-            var note = new Object()
+
             var reader = new FileReader();
 
-            reader.onload = function(e) {
-                result = reader.result;
+            // Closure to capture the file information.
+            reader.onload = (function (theFile) {
+                return function (e) {
+                    processedCount++;
+                    result = e.target.result;
+                    note = new Object();
 
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(result, "text/html");
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(result, "text/html");
 
-                note.title = doc.getElementsByClassName('title')[0].textContent;
-                note.content = doc.getElementsByClassName('content')[0].textContent;
-                note.labels = Array.from(doc.getElementsByClassName('label')).map(function(e){
-                    return e.textContent;
-                });
-                output.push(note);
-            }
-            reader.readAsText(f);	
+                    if(doc.getElementsByClassName('title').length > 0){
+                        note.title = doc.getElementsByClassName('title')[0].textContent;
+                    }
+                    
+                    if(doc.getElementsByClassName('content').length > 0){
+                        note.content = doc.getElementsByClassName('content')[0].textContent;
+                    }
+                    
+                    if(doc.getElementsByClassName('label').length > 0){
+                        note.labels = Array.from(doc.getElementsByClassName('label')).map(function (e) {
+                            return e.textContent;
+                        });
+                    }
+                    output.push(note); 
+                    
+                    if(processedCount == totalFiles){
+                        console.log('OK');
+                        document.getElementById('fileDisplayArea').innerHTML = '<ul>' + output.join('') + '</ul>';
+                    }
+                    
+                };
+            })(f);
+
+            reader.readAsText(f);
 
         } else {
             fileDisplayArea.innerText = "File not supported!"
