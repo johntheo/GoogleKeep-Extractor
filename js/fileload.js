417031b17daffdcc49697c0124485de7b78c9300
John Theo Sierpinski de Souza (BFSI - Brazil) <JO389506@wipro.com>
2018-01-25 17:52:01 -0200

    Reading files. Crashing for multiple files yet

--- js/fileload.js
+++ js/fileload.js
@@ -1,14 +1,37 @@
+var notes = []
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
+    var textType = /text.*/;
 
     // files is a FileList of File objects. List some properties.
     var output = [];
     for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate.toLocaleDateString(), '</li>');
+        var result = "";
+        if (f.type.match(textType)) {
+            var note = new Object()
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                result = reader.result;
+
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(result, "text/html");
+
+                note.title = doc.getElementsByClassName('title')[0].textContent;
+                note.content = doc.getElementsByClassName('content')[0].textContent;
+                note.labels = Array.from(doc.getElementsByClassName('label')).map(function(e){
+                    return e.textContent;
+                });
+                output.push(note);
+            }
+            reader.readAsText(f);	
+
+        } else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
     }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    //fileDisplayArea.innerText = '<ul>' + output.join('') + '</ul>';
+    document.getElementById('fileDisplayArea').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
